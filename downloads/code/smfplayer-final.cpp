eb68f37ba5500f1ad1984f02b623c775aa8e667f
Shun Terabayashi <shunonymous@gmail.com>
2019-02-16 10:50:09 +0900

    Site updated: 2019-02-16 10:50:08

---
+++ downloads/code/smfplayer-final.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <functional>
+
+#include <fluidsynth.h>
+
+int main(int argc, char** argv)
+{
+    fluid_settings_t* settings;
+    fluid_player_t* player;
+    fluid_audio_driver_t* adriver;
+    fluid_synth_t* synth;
+    fluid_midi_event_t* event;
+
+    std::function handler = static_cast<int(*)(void*, fluid_midi_event_t*)>(
+	[](void* synth, fluid_midi_event_t* event){
+	    fluid_synth_handle_midi_event(synth, event);
+	    std::cout << fluid_midi_event_get_type(event) << "\n" << std::flush;
+	    return 0;
+	});
+
+    settings = new_fluid_settings();
+    fluid_settings_setstr(settings, "audio.driver", "pulseaudio");
+    synth = new_fluid_synth(settings);
+    player = new_fluid_player(synth);
+    adriver = new_fluid_audio_driver(settings, synth);
+    fluid_player_set_playback_callback(player, *handler.target<int(*)(void*, fluid_midi_event_t*)>(), synth);
+    
+    /* process command line arguments */
+    for (int i = 1; i < argc; i++) {
+        if (fluid_is_soundfont(argv[i])) {
+	    fluid_synth_sfload(synth, argv[1], 1);
+        }
+        if (fluid_is_midifile(argv[i])) {
+            fluid_player_add(player, argv[i]);
+        }
+    }
+
+    fluid_player_play(player);
+    fluid_player_join(player);
+    delete_fluid_player(player);
+    delete_fluid_audio_driver(adriver);
+    delete_fluid_synth(synth);
+    delete_fluid_settings(settings);
+}
+    
